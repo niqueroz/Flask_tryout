db13e7917523e605a824c73a17653356b95fdad1
Annique Rozenberg <anniquerozenberg@hotmail.com>
2024-05-28 21:47:55 +0200

    versie zonder hashtag

--- flask_app.py
+++ flask_app.py
@@ -7,8 +7,7 @@ app=Flask(__name__)
 @app.route('/')
 def home ():
     return "<*Yellow*>Hello world!<*curled*>"
-
-#controleren of wel update komt 
+ 
 
 if __name__== '__main__':
    app.run(port=5000,debug=True)
